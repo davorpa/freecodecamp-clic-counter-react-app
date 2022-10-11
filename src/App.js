c8b13eef48a3308bcdf902793097e2e892102ee0
David Ordás <3125580+davorpa@users.noreply.github.com>
2022-10-11 17:11:08 +0200

    feat: add fcc logo

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
+import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-    return <div className="App"></div>;
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+            </header>
+        </div>
+    );
 }
 
 export default App;
